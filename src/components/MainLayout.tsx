ab7073d04fc2c2c2cd324eb7af59f2983d7cb851
Azuar <azuarok@gmail.com>
2025-01-27 18:10:10 +0700

    validation user role

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -10,64 +10,103 @@ import {
   SettingOutlined,
   HistoryOutlined,
   LogoutOutlined,
+  TeamOutlined,
 } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import type { MenuProps } from "antd";
 import { Layout, Menu } from "antd";
 
-const { Content, Footer, Sider } = Layout;
-
-type MenuItem = Required<MenuProps>["items"][number];
-
-function getItem(
-  label: React.ReactNode,
-  key: React.Key,
-  icon?: React.ReactNode,
-  children?: MenuItem[]
-): MenuItem {
-  return {
-    key,
-    icon,
-    children,
-    label,
-  } as MenuItem;
-}
-
-const logout = () => {
-  localStorage.clear();
-};
-
-const items: MenuItem[] = [
-  getItem(<Link to="/dashboard">Dashboard</Link>, "1", <PieChartOutlined />),
-  getItem("Mahasiswa", "sub1", <UserOutlined />, [
-    getItem(<Link to="/profil">Profil</Link>, "2", <UserOutlined />),
-    getItem(
-      <Link to="/judul-skripsi">Judul Skripsi</Link>,
-      "3",
-      <ClusterOutlined />
-    ),
-    getItem(<Link to="/seminar">Cat. Seminar</Link>, "4", <ScheduleOutlined />),
-    getItem(
-      <Link to="/sidang">Cat. Sidang</Link>,
-      "5",
-      <ReconciliationOutlined />
-    ),
-  ]),
-  getItem("Settings", "sub2", <SettingOutlined />, [
-    getItem("Ganti Password", "6", <HistoryOutlined />),
-    getItem(
-      <Link to="/login" onClick={logout}>
-        Logout
-      </Link>,
-      "7",
-      <LogoutOutlined />
-    ),
-  ]),
-];
-
 const MainLayout: React.FC = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const { Content, Footer, Sider } = Layout;
 
+  const user: any = localStorage.getItem("user");
+  const userData = JSON.parse(user);
+
+  type MenuItem = Required<MenuProps>["items"][number];
+
+  function getItem(
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[]
+  ): MenuItem {
+    return {
+      key,
+      icon,
+      children,
+      label,
+    } as MenuItem;
+  }
+
+  const logout = () => {
+    localStorage.clear();
+  };
+
+  const items: MenuItem[] = [
+    getItem(<Link to="/dashboard">Dashboard</Link>, "1", <PieChartOutlined />),
+    userData.data.role === "mahasiswa"
+      ? getItem("Mahasiswa", "sub1", <UserOutlined />, [
+          getItem(<Link to="/profil">Profil</Link>, "2", <UserOutlined />),
+          getItem(
+            <Link to="/judul-skripsi">Judul Skripsi</Link>,
+            "3",
+            <ClusterOutlined />
+          ),
+          getItem(
+            <Link to="/seminar">Cat. Seminar</Link>,
+            "4",
+            <ScheduleOutlined />
+          ),
+          getItem(
+            <Link to="/sidang">Cat. Sidang</Link>,
+            "5",
+            <ReconciliationOutlined />
+          ),
+        ])
+      : userData.data.role === "dosen"
+      ? getItem("Dosen", "sub1", <UserOutlined />, [
+          getItem(<Link to="/profil">Profil</Link>, "2", <UserOutlined />),
+          getItem(
+            <Link to="/judul-skripsi">Judul Skripsi</Link>,
+            "3",
+            <ClusterOutlined />
+          ),
+          getItem(
+            <Link to="/seminar">Cat. Seminar</Link>,
+            "4",
+            <ScheduleOutlined />
+          ),
+          getItem(
+            <Link to="/sidang">Cat. Sidang</Link>,
+            "5",
+            <ReconciliationOutlined />
+          ),
+        ])
+      : getItem("Admin", "sub1", <UserOutlined />, [
+          getItem(<Link to="/dosen">Dosen</Link>, "2", <TeamOutlined />),
+          getItem(
+            <Link to="/seminar">Cat. Seminar</Link>,
+            "3",
+            <ScheduleOutlined />
+          ),
+          getItem(
+            <Link to="/sidang">Cat. Sidang</Link>,
+            "4",
+            <ReconciliationOutlined />
+          ),
+        ]),
+    getItem("Settings", "sub2", <SettingOutlined />, [
+      getItem("Ganti Password", "6", <HistoryOutlined />),
+      getItem(
+        <Link to="/login" onClick={logout}>
+          Logout
+        </Link>,
+        "7",
+        <LogoutOutlined />
+      ),
+    ]),
+  ];
   return (
     <Layout
       style={{
